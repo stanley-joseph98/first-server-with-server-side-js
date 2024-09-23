91041b5b1bc2f45ed8f9997be304ddbff25bf179
joseph-ouma <josekouma@gmail.com>
2024-09-23 16:28:42 +0300

    Asynchronous Callback

--- CD220Labs/http_server/today.js
+++
@@ -1,6 +0,0 @@
- // Export a function named 'getDate' from the module
-module.exports.getDate = function getDate() {
-    // Get the current date and time in the timezone "Australia/Brisbane"
-    let aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"});
-    return aestTime; // Return the formatted date and time
-};
